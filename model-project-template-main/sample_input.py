ae91dd2ffbd4203d1b7eaf6e80b04d16bdcd9642
Adam <adamwqian@gmail.com>
2022-12-05 19:18:46 -0500

    Build props from sample input

--- model-project-template-main/sample_input.py
+++ model-project-template-main/sample_input.py
@@ -8,6 +8,10 @@ def get_sample_by_no(number):
                         "2022-12-01":[
                             ['12:00:00','F'],
                             ['15:00:00','T']
+                        ],
+                        "2022-12-02":[
+                            ['12:00:00','T'],
+                            ['15:00:00','T']
                         ]
                     }
                 }
@@ -21,6 +25,10 @@ def get_sample_by_no(number):
                 "2022-12-01":[
                     ['12:00:00','light rain', 'F'],
                     ['15:00:00', 'few clouds', 'T']
+                ],
+                '2022-12-02': [
+                    ['12:00:00','light rain', 'F'],
+                    ['15:00:00', 'few clouds', 'T']
                 ]
             }
         }
@@ -28,17 +36,39 @@ def get_sample_by_no(number):
     return sample
 
 if __name__ == "__main__":
-    using_sample_input = input("Enter sample number to use sample input. For manual input, press Enter: ")
-    if using_sample_input == "1":
-        samples = get_sample_by_no(1)
-        print(samples['attendees'])
-    else:
-        print('manual')
+    # using_sample_input = input("Enter sample number to use sample input. For manual input, press Enter: ")
+    # if using_sample_input == "1":
+    #     samples = get_sample_by_no(1)
+    #     print(samples['attendees'])
+    # else:
+    #     print('manual')
 
-    xd = {
-        'item1':'hi',
-        'item2':'hi'
-    }
+    samples = get_sample_by_no(1)
 
-    for count, val in enumerate(xd):
-        print(count, val)
+    # weather props
+    time_idx = 0
+    for date in samples['weather']:
+        for time in samples['weather'][date]:
+            print(time_idx, time)
+            time_idx += 1
+
+            #Assume WeatherProp(date, time, index, is_clear)
+            #PROPS.append(WeatherProp(date, time[0], idx, time[2] == 'T'))
+    
+    # timing props
+    time_idx = 0
+    for personidx, person in enumerate(samples['attendees']):
+        for date in person['avail']:
+            for time in person['avail'][date]:
+                print(time_idx, time)
+                time_idx += 1
+
+                # Assume TimingProp(date, time, time_index, person, is_available)
+                #PROPS.append(TimingProp(date, time[0], time_idx, personidx, time_idx[1]))
+
+    # indoor/outdoor props
+    for idx, activity in enumerate(samples['activities']):
+        print(idx, activity)
+
+        # Assume IndoorsProps(activity_name, activity_index, is_indoors)
+        #PROPS.append(IndoorsProps(activity['name'], activity['indoors'] = True))
